749040694dbaf082920265c05e53b4e47e623b42
Oscar González Baldizon <oscar.gonzalez.baldizon@gmail.com>
2024-05-27 21:42:00 -0600

    summit

---
+++ ExercisesMultipleLanguages/C/NumEntero.c
@@ -0,0 +1,12 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	n = int()
+	print("Ingresa un numero entero: ")
+	n = int(input())
+	if n>0:
+		print("Numero es positivo y mayor a cero (0)")
+
---
+++ ExercisesMultipleLanguages/Pseudocodigo/Numero_Entero.psc
@@ -0,0 +1,4 @@
+Proceso Numero_Entero
+	Definir n Como Entero
+	escribir "Ingresa un numero entero: ";
+FinProceso
